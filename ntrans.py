9d1e1a91c88baf0ca3f22db1c368c2e1e55b1cf3
ThePhilgrim <76887896+ThePhilgrim@users.noreply.github.com>
2021-05-19 23:03:14 +0200

    Delete all logic that has been moved to ntrans_dataprep

--- ntrans.py
+++ ntrans.py
@@ -1,34 +1,13 @@
-import nltk
-import translatepy
-import re
-import collections
-import csv
-# import ssl
+# import translatepy
+# import csv
 
+"""
+DEVELOPER INFO:
 
-def prepare_corpus_data(data_size=1000000):
-    # TODO: Put code that downloads BNC here
+The usage of this script relies on the N-gram files in "./ngrams"
 
-    bnc_corpus = nltk.corpus.BNCCorpusReader(root='BNC/Texts/', fileids=r'[A-K]/\w*/\w*\.xml')
-
-    bnc_sents = []
-
-    # TODO:
-    # If user wants a smaller data set than the full corpus, it would be better
-    # to pick random sents() from bnc_corpus instead of for looping over the first
-    # X sents. If data_size = 10.000, pick 10.000 random sents from bnc_corpus
-
-    for count, sentence in enumerate(bnc_corpus.sents()):
-        string_sent = ' '.join(sentence)
-        bnc_sents.append(string_sent)
-        if count == 1000000:  # TODO: Change count == 10000 to "count == data_size"
-            print(count)
-            break
-        # TODO: Remake print(count) to progress bar in tkinter
-        if count % 1000 == 0:
-            print(count)
-
-    return ' '.join(bnc_sents)
+These files are created through ntrans_dataprep.py
+"""
 
 
 def create_csv_file(source_target_pairs, save_path=None):
@@ -63,45 +42,7 @@ def machine_translate_ngrams(list_of_ngrams, language=None):
     create_csv_file(source_target_pairs)
 
 
-def count_ngram_frequency(ngram_strings, desired_data_size):
-    x_most_common_ngrams = collections.Counter(ngram_strings).most_common(desired_data_size)
-    return [ngram[0] for ngram in x_most_common_ngrams]
-
-
-def create_ngrams(corpus_data, ngram_size, desired_data_size):
-    """
-    Strips string from special characters, creates tuple n_grams, deletes tuples
-    with numbers, joins tuples into strings
-    """
-
-    # Removes punctuation and special characters from string.
-    text_without_punctuation = re.sub(r"[^\w']+", " ", corpus_data).lower()
-
-    # TODO: Make the .replaces more effective
-    text_without_punctuation = text_without_punctuation.replace(" '", "'")
-    text_without_punctuation = text_without_punctuation.replace(" n't", "n't")
-
-    # TODO: Put ngram function in for loop to include all N-grams that the user wants.
-    # TODO: Switch place of ngram creation and joining ngram_tuples into strings, since joining ngram_tuples takes a long time.
-    # Splits string into N-grams and adds them to tuple_list.
-    generated_ngram_tuples = nltk.ngrams(text_without_punctuation.split(), ngram_size)
-
-    # Joins tuples into strings, and deletes strings with numbers.
-    # TODO: Takes long time. Possible to make faster?
-    ngram_strings = []
-    print('Joining n-gram tuples to strings')
-    for generated_ngram_tuple in generated_ngram_tuples:
-        joined_tuple = " ".join(generated_ngram_tuple)
-        if not any(char.isdigit() for char in joined_tuple):
-            ngram_strings.append(joined_tuple)
-
-    count_ngram_frequency(ngram_strings, desired_data_size)
-    # # return machine_translate_ngrams(count_ngram_frequency(ngram_strings, desired_data_size))
-
 
 """
 CODE TESTING:
 """
-
-corpus_data = prepare_corpus_data()
-create_ngrams(corpus_data, 2, 100)
