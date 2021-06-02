09a248a53b1d5c5403c67bf916f6b1f34b6a9946
ThePhilgrim <76887896+ThePhilgrim@users.noreply.github.com>
2021-06-02 23:59:16 +0200

    Improve var names and delete redundant comments

--- ntrans_gui.py
+++ ntrans_gui.py
@@ -11,8 +11,7 @@ class NTransMainGui:
     """
 
     def __init__(self):
-        # User-determined variables
-        self.included_ngrams = [2, 3, 4, 5, 6]
+        self.user_chosen_ngrams = [2, 3, 4, 5, 6]
 
         # Window & Frame
         self.root = tkinter.Tk()
@@ -22,7 +21,6 @@ class NTransMainGui:
         mainframe = ttk.Frame(self.root)
         mainframe.pack(fill="both", expand=True)
 
-        # Header
         header = ttk.Label(
             mainframe,
             text="N-Trans Dictionary Settings",
@@ -122,65 +120,55 @@ class NTransMainGui:
         )
 
         # About / Help
-
-        # To fit About & Help button in same column
-        button_frame = ttk.Frame(mainframe)
+        about_help_buttonframe = ttk.Frame(mainframe)
 
         about_button = ttk.Button(
-            button_frame, command=self.open_about_window, text="About", style=""
+            about_help_buttonframe,
+            command=self.open_about_window,
+            text="About",
+            style="",
         )  # TODO: Make command into lambda instantiating about-class
-        help_button = ttk.Button(button_frame, command=self.open_help_page, text="Help")
+        help_button = ttk.Button(
+            about_help_buttonframe, command=self.open_help_page, text="Help"
+        )
 
-        # Widget Positioning
+        # Black turn off formatting
+        # fmt: off
+
+        # ==================
+        # Widget placement
+        # ==================
         header.grid(column=0, row=0, columnspan=2, padx=(0, 0), pady=(30, 30))
 
         ngram_check_label.grid(sticky="N", column=0, row=1, padx=(20, 0), pady=(10, 10))
 
-        select_all_checkbutton.grid(
-            sticky="W", column=0, row=2, padx=(40, 0), pady=(0, 10)
-        )
-        n_to_ngram_checkbox[2].grid(
-            sticky="W", column=0, row=3, padx=(40, 0), pady=(0, 0)
-        )
-        n_to_ngram_checkbox[3].grid(
-            sticky="W", column=0, row=4, padx=(40, 0), pady=(0, 0)
-        )
-        n_to_ngram_checkbox[4].grid(
-            sticky="W", column=0, row=5, padx=(40, 0), pady=(0, 0)
-        )
-        n_to_ngram_checkbox[5].grid(
-            sticky="W", column=0, row=6, padx=(40, 0), pady=(0, 0)
-        )
-        n_to_ngram_checkbox[6].grid(
-            sticky="W", column=0, row=7, padx=(40, 0), pady=(0, 0)
-        )
+        select_all_checkbutton.grid(sticky="W", column=0, row=2, padx=(40, 0), pady=(0, 10))
+        n_to_ngram_checkbox[2].grid(sticky="W", column=0, row=3, padx=(40, 0), pady=(0, 0))
+        n_to_ngram_checkbox[3].grid(sticky="W", column=0, row=4, padx=(40, 0), pady=(0, 0))
+        n_to_ngram_checkbox[4].grid(sticky="W", column=0, row=5, padx=(40, 0), pady=(0, 0))
+        n_to_ngram_checkbox[5].grid(sticky="W", column=0, row=6, padx=(40, 0), pady=(0, 0))
+        n_to_ngram_checkbox[6].grid(sticky="W", column=0, row=7, padx=(40, 0), pady=(0, 0))
 
         data_size_label.grid(sticky="W", column=0, row=8, padx=(20, 0), pady=(30, 0))
         ngram_data_size.grid(sticky="W", column=0, row=9, padx=(20, 0), pady=(5, 0))
 
-        target_language_label.grid(
-            sticky="W", column=0, row=10, padx=(20, 20), pady=(30, 0)
-        )
+        target_language_label.grid(sticky="W", column=0, row=10, padx=(20, 20), pady=(30, 0))
         target_language.grid(sticky="W", column=0, row=11, padx=(20, 20), pady=(0, 0))
 
         filepath_label.grid(sticky="W", column=0, row=12, padx=(20, 0), pady=(30, 10))
         show_filepath.grid(sticky="W", column=0, row=13, padx=(20, 0), pady=(0, 10))
-        get_savepath_button.grid(
-            sticky="W", column=1, row=13, padx=(0, 20), pady=(0, 10)
-        )
+        get_savepath_button.grid(sticky="W", column=1, row=13, padx=(0, 20), pady=(0, 10))
 
-        generate_dictionary.grid(
-            column=0, row=14, columnspan=2, padx=(0, 0), pady=(30, 30)
-        )
+        generate_dictionary.grid(column=0, row=14, columnspan=2, padx=(0, 0), pady=(30, 30))
 
-        button_frame.grid(
-            sticky="E", column=0, row=15, columnspan=2, padx=(20, 20), pady=(0, 10)
-        )
+        about_help_buttonframe.grid(sticky="E", column=0, row=15, columnspan=2, padx=(20, 20), pady=(0, 10))
         about_button.pack(side="left")
         help_button.pack(side="right")
 
+        # Black turn on formatting
+        # fmt: on
+
     def get_save_file_path(self):
-        # TODO: Check filepath for validity
         self.savepath = filedialog.askdirectory()
         if self.savepath:
             self.filepath.set(self.savepath)
@@ -188,7 +176,7 @@ class NTransMainGui:
     def generate_ntrans_dictionary(self):
         self.user_choices = {
             "save_path": self.filepath.get(),
-            "included_ngrams": self.included_ngrams,
+            "included_ngrams": self.user_chosen_ngrams,
             "amount_of_ngrams": self.data_size_var.get(),
             "target_language": self.target_language_var.get(),
         }
@@ -215,13 +203,13 @@ class NTransMainGui:
                 self.checkbox_vars[n].set(True)
 
             for n in range(2, 7):
-                if n not in self.included_ngrams:
-                    self.included_ngrams.append(n)
+                if n not in self.user_chosen_ngrams:
+                    self.user_chosen_ngrams.append(n)
         else:
             for n in self.checkbox_vars:
                 self.checkbox_vars[n].set(False)
 
-            self.included_ngrams.clear()
+            self.user_chosen_ngrams.clear()
 
     def open_about_window(self):
         self.about_window = AboutWindow()
@@ -232,11 +220,11 @@ class NTransMainGui:
     def update_ngram_checkbox(self, n):
         if self.checkbox_vars[n].get():
             self.checkbox_vars[n].set(True)
-            self.included_ngrams.append(n)
+            self.user_chosen_ngrams.append(n)
         else:
             self.checkbox_vars[n].set(False)
             self.select_all_var.set(False)
-            self.included_ngrams.remove(n)
+            self.user_chosen_ngrams.remove(n)
 
 
 class AboutWindow:
